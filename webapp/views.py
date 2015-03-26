0194d77bd17cce76535bad33e3114fc46352165f
dsv_git <dsv.mail@yandex.ru>
2015-03-26 04:55:22 +0300

    add views with bootstrap

--- webapp/views.py
+++ webapp/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render_to_response
 from webapp import models
 
 def index(request):
-	return render_to_response('index.html', {'title':'kormushka'})
+	return render_to_response('list.html', {'title':'kormushka'})
 
